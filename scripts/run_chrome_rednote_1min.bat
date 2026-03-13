e6606f6da4b672d02a175d5f3c9bae645dafffcd
xubin <xubin@zhidemai.com>
2026-03-13 13:17:56 +0800

    chrome定时重启脚本

---
+++ scripts/run_chrome_rednote_1min.bat
@@ -0,0 +1,21 @@
+@echo off
+chcp 65001 >nul
+set CHROME_EXE="C:\Program Files\Google\Chrome\Application\chrome.exe"
+set PROFILE_NAME=temp1
+set USER_DATA=c:/chrome-spider/%PROFILE_NAME%
+set URL=https://www.rednote.com/explore
+
+:loop
+echo [%date% %time%] 启动 Chrome...
+start "" %CHROME_EXE% --user-data-dir="%USER_DATA%" --new-tab %URL%
+
+echo 等待 1800 秒...
+timeout /t 1800 /nobreak
+
+echo [%date% %time%] 关闭当前实例 Chrome（仅 user-data-dir 含 %PROFILE_NAME% 的进程）...
+powershell -NoProfile -Command "$p='%PROFILE_NAME%'; Get-CimInstance Win32_Process -Filter \"name='chrome.exe'\" | Where-Object { $_.CommandLine -like \"*$p*\" } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force -ErrorAction SilentlyContinue }"
+
+echo 等待 2 秒后重新打开...
+timeout /t 2 /nobreak
+
+goto loop
